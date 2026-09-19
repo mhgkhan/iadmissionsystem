61cb90ce3d4a3798ff4d74bf4ad025709471163c
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2026-09-19 05:17:32 -0700

    remmove server.js

--- api/index.js
+++ api/index.js
@@ -25,5 +25,18 @@ APP.get("/", (req, res) => {
 })
 
 
+const PORT = process.env.PORT || 3000;
 
+
+try {
+    await mongoose.connect("mongodb://localhost:27017/iadmissionsystem");
+
+    APP.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+} catch (error) {
+    console.log("Error in connecting to database:", error);
+}
+
+export default APP;
 
